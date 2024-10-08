6cd95ebb78591047ad902949ff1471daaee4c853
Abigail Gillespie <ag00137@my.westga.edu>
2024-10-08 13:20:34 -0400

    Created the Model namespace and a class in the namespace called Car. Then declared the make, model, mpg, and price properties. Also created a constructor that accepts the values and initializes the properties.

---
+++ AbbieGillespieProject1/AbbieGillespieProject1/Model/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbbieGillespieProject1.Model
+{
+    public class Car
+    {
+        public string? make {  get; set; }
+
+        public string? model { get; set; }
+
+        public decimal? mpg { get; set; }
+
+        public decimal? price { get; set; }
+
+        public Car(string make, string model, decimal mpg, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(make) || string.IsNullOrWhiteSpace(model))
+            {
+                throw new ArgumentException("The make and model of the car cannot be empty.");
+            }
+            else if (mpg <= 0 || price <= 0)
+            {
+                throw new ArgumentException("MPG or price cannot be empty.");
+            }
+
+            string Make = make;
+            string  Model = model;
+            decimal Mpg = mpg;
+            decimal Price = price;
+        }
+    }
+}
