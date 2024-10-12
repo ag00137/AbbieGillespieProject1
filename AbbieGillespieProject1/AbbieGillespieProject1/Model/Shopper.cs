4cf98f97d43765da59e642fe0f1ca779a7400f40
Abigail Gillespie <ag00137@my.westga.edu>
2024-10-12 10:58:35 -0400

    Created a new class called Shopper in the Model namespace and declared name and MoneyAvailable properties. Then added a private List for the Cars and a constructor. Also a CanPurchase method and a PurchaseCar method.

---
+++ AbbieGillespieProject1/AbbieGillespieProject1/Model/Shopper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbbieGillespieProject1.Model
+{
+    /// <summary>
+    /// Sees if a shopper can buy a car.
+    /// </summary>
+    public class Shopper
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the money available.
+        /// </summary>
+        public decimal MoneyAvailable { get; set; }
+
+        private List<Car> _cars;
+
+        /// <summary>
+        /// Initializes a new instance of the shopper class.
+        /// </summary>
+        /// <param name="name">The name of the shopper</param>
+        /// <param name="moneyAvailable">The money the shopper has available.</param>
+        /// <exception cref="System.ArgumentException">Invalid details.</exception>
+        public Shopper(string name, decimal moneyAvailable)
+        {
+            if (string.IsNullOrWhiteSpace(name) || moneyAvailable <= 0) throw new ArgumentException("Invalid details.");
+
+            Name = name;
+            MoneyAvailable = moneyAvailable;
+            _cars = new List<Car>();
+        }
+
+        /// <summary>
+        /// Determines whether this instance can purchase the specified car.
+        /// </summary>
+        /// <param name="car">The car.</param>
+        /// <returns>
+        ///   <c>true</c> if this instance can purchase the specified car; otherwise, <c>false</c>.
+        /// </returns>
+        public bool CanPurchase (Car car)
+        {
+            decimal totalCost = car.price + (car.price * CarLot.TAX_RATE);
+            if (MoneyAvailable >= totalCost)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Purchases the car.
+        /// </summary>
+        /// <param name="car">The car the shopper wants</param>
+        public void PurchaseCar(Car car)
+        {
+            decimal totalCost = car.price + (car.price * CarLot.TAX_RATE);
+            if (CanPurchase(car))
+            {
+                _cars.Add(car);
+                MoneyAvailable -= totalCost;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of cars.
+        /// </summary>
+        public List<Car> Cars => _cars;
+    }
+}
